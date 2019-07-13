0c28633b9c94af620da4d056c32c2af73109f1e7
Bryan Lynn <lynnbry@oregonstate.edu>
2019-07-13 12:37:18 -0700

    error with 'a' variable being defined twice. (once as part of linear growth func. and once as the constant in the quadratic equation)

--- code/bacpo_calccond.py
+++ code/bacpo_calccond.py
@@ -3,12 +3,12 @@ import math
 
 
 #constants
-a=1.0
+a=5.0
 m=8.0
-d=0.01
+d=0.05
 q1=0.24
 q2=0.25
-k1=0.015
+k1=0.01
 s0=1.0
 
 
@@ -24,17 +24,17 @@ def f(s,e):
     growthfunc=a*s*e #linear growth func
     return growthfunc
 
-#det s compatibility s^2*q1-q1*(s0-c)+(da/(1-q2))=0
+#det s compatibility q1*s^2-q1*(s0-c)*s+(da/(1-q2))=0
 def rad_s(a,q1,q2,c,d,s0):
-    bee=float(q1*(c-s0)) #b in quadratic formula as^2+bs+c=0
-    cee=float((d*a)/(1-q2)) #c in quadratic formula
-    radicand= float(bee*bee - (4*a*cee)) #needs to be pos for real solutions
+    bee=float(q1*(c-s0)) #b in quadratic formula ds^2+bs+c=0
+    cee=float(d/((1-q2)*a)) #c in quadratic formula
+    radicand= float(bee*bee - (4*cee*q1)) #needs to be pos for real solutions
     return radicand
 
-def solve_s(a,q1,q2,d,k1,s0,c,radicand):
+def solve_s(q1,q2,d,k1,s0,c,radicand):
     bee=float(q1*(c-s0))
-    sol1= ((-bee)+(math.sqrt(radicand)))/(2*a)
-    sol2= ((-bee)-(math.sqrt(radicand)))/(2*a)
+    sol1= ((-bee)+(math.sqrt(radicand)))/(2*q1)
+    sol2= ((-bee)-(math.sqrt(radicand)))/(2*q1)
     print "sol1: "
     steady_states(q1,q2,d,k1,s0,c,sol1)
     print "sol2: "
@@ -45,12 +45,7 @@ def steady_states(q1,q2,d,k1,s0,c,s):
     x2_stst=((q2-q1)*d)/(q2*k1)
     e_stst=(q1*(s0-s-c))
     x1_stst= (e_stst*(1-q2))/q1
-<<<<<<< HEAD
-    print "s: ", s, "e: ", e_stst, "x1: ", x1_stst, "x2: ",x2_stst
-            
-=======
-    print "x2: ",x2_stst, "e: ", e_stst, "x1: ", x1_stst, "s: ", s
->>>>>>> ec90ec1b27adb4d1cd0b3b74aa55063f0c89b6e9
+    print "s: ", s, "e: ", e_stst, "x1: ", x1_stst, "x2: ",x2_stst            
     
 
 c=calc_constant(q1,q2,d,k1)
@@ -60,11 +55,9 @@ print "c : ", c
 print "radicand : ", rad
 
 
-solve_s(a,q1,q2,d,k1,s0,c,rad)
+solve_s(q1,q2,d,k1,s0,c,rad)
 
-<<<<<<< HEAD
 print "constants: "
 print "a: ", a, "m: ", m, "d: ", d, "q1: ", q1, "q2: ", q2, "k1: ", k1, "s0: ", s0
-=======
->>>>>>> ec90ec1b27adb4d1cd0b3b74aa55063f0c89b6e9
+
 
